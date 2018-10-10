1b6e93b6e5c5aa52a1113f4c0634878bfedba1a4
Sebastian Staszyński <sebastian.staszynski@jitsolutions.pl>
2018-10-10 13:05:07 +0200

    Sample Unit test

--- app/src/test/java/pl/jitsolutions/agile/presentation/login/LoginViewModelTest.kt
+++ app/src/test/java/pl/jitsolutions/agile/presentation/login/LoginViewModelTest.kt
@@ -2,44 +2,33 @@ package pl.jitsolutions.agile.presentation.login
 
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
-import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.Dispatchers
 import kotlinx.coroutines.experimental.channels.produce
+import kotlinx.coroutines.experimental.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import pl.jitsolutions.agile.domain.LoginUserUseCase
 import pl.jitsolutions.agile.domain.User
 import pl.jitsolutions.agile.repository.ProjectRepository
 import pl.jitsolutions.agile.repository.UserRepository
-import java.lang.Runnable
-import kotlin.coroutines.experimental.CoroutineContext
 
 class LoginViewModelTest {
 
     @Test
-    fun doAction_doesSomething() {
-
+    fun doAction_doesSomething() = runBlocking<Unit>(Dispatchers.Default) {
         val mockUserRepository = mock<UserRepository> {
-            on { login("abc", "123") } doReturn CoroutineScope(TestDispatcher()).produce {
-                println("send")
-                send(User("abc"))
-            }
+            on { login("abc", "123") } doReturn produce { send(User("abc")) }
         }
+
         val mockProjectRepository = mock<ProjectRepository> {
-
+            on { getGroups("abc") } doReturn produce { send("Test group") }
         }
 
-        val classUnderTest = LoginUserUseCase(mockUserRepository, mockProjectRepository, TestDispatcher())
-        GlobalScope.launch {
-            val login = classUnderTest.execute("abc", "123")
-        }
+        val classUnderTest = LoginUserUseCase(mockUserRepository, mockProjectRepository, Dispatchers.Unconfined)
 
-        assertEquals(true, true)
-    }
-}
+        val channel = classUnderTest.execute("abc", "123")
+        val userName = channel.receive()
 
-class TestDispatcher : CoroutineDispatcher() {
-
-    override fun dispatch(context: CoroutineContext, block: Runnable) {
-        Dispatchers.Default.dispatch(context, block)
+        assertEquals("abc, groups: Test group", userName)
     }
 }
