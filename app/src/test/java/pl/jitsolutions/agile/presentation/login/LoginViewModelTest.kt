68c795d56dc48c1fc0b50527402722d5b277d32d
Sebastian Staszyński <sebastian.staszynski@jitsolutions.pl>
2018-10-11 10:46:27 +0200

    Fix test

--- app/src/test/java/pl/jitsolutions/agile/presentation/login/LoginViewModelTest.kt
+++ app/src/test/java/pl/jitsolutions/agile/presentation/login/LoginViewModelTest.kt
@@ -8,7 +8,9 @@ import kotlinx.coroutines.experimental.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import pl.jitsolutions.agile.domain.LoginUserUseCase
+import pl.jitsolutions.agile.domain.Response
 import pl.jitsolutions.agile.domain.User
+import pl.jitsolutions.agile.domain.response
 import pl.jitsolutions.agile.repository.ProjectRepository
 import pl.jitsolutions.agile.repository.UserRepository
 
@@ -17,18 +19,20 @@ class LoginViewModelTest {
     @Test
     fun doAction_doesSomething() = runBlocking<Unit>(Dispatchers.Default) {
         val mockUserRepository = mock<UserRepository> {
-            on { login("abc", "123") } doReturn produce { send(User("abc")) }
+            on { login("abc", "123") } doReturn produce { send(response(User("abc"))) }
         }
 
         val mockProjectRepository = mock<ProjectRepository> {
-            on { getGroups("abc") } doReturn produce { send("Test group") }
+            on { getGroups("abc") } doReturn produce { send(response("Test group")) }
         }
 
         val classUnderTest = LoginUserUseCase(mockUserRepository, mockProjectRepository, Dispatchers.Unconfined)
         val params = LoginUserUseCase.Params("abc", "123")
         val channel = classUnderTest.execute(params)
 
-        val userName = channel.receive()
-        assertEquals("abc, groups: Test group", userName)
+        val respose = channel.receive()
+
+        assertEquals(Response.Status.SUCCESS, respose.status)
+        assertEquals("abc, groups: Test group", respose.data)
     }
 }
