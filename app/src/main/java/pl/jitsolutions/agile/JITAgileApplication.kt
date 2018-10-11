6affc8615836723db209b350b16a595064aab0ac
Sebastian Staszyński <sebastian.staszynski@jitsolutions.pl>
2018-10-11 11:27:52 +0200

    Setup crashlytics for production builds

--- app/src/main/java/pl/jitsolutions/agile/JITAgileApplication.kt
+++ app/src/main/java/pl/jitsolutions/agile/JITAgileApplication.kt
@@ -1,6 +1,9 @@
 package pl.jitsolutions.agile
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
+import io.fabric.sdk.android.Fabric
 import kotlinx.coroutines.experimental.CoroutineDispatcher
 import kotlinx.coroutines.experimental.Dispatchers
 import org.kodein.di.Kodein
@@ -11,7 +14,8 @@ import org.kodein.di.generic.provider
 import org.kodein.di.generic.singleton
 import pl.jitsolutions.agile.domain.LoginUserUseCase
 import pl.jitsolutions.agile.presentation.login.LoginViewModelFactory
-import pl.jitsolutions.agile.repository.*
+import pl.jitsolutions.agile.repository.FirebaseUserRepository
+import pl.jitsolutions.agile.repository.MockProjectRepository
 
 class JITAgileApplication : Application(), KodeinAware {
     override val kodein = Kodein.lazy {
@@ -20,5 +24,18 @@ class JITAgileApplication : Application(), KodeinAware {
         bind<MockProjectRepository>() with singleton { MockProjectRepository(instance()) }
         bind<LoginUserUseCase>() with provider { LoginUserUseCase(instance(), instance(), instance()) }
         bind<LoginViewModelFactory>() with provider { LoginViewModelFactory(instance()) }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        setupCrashlytics()
+    }
+
+    private fun setupCrashlytics() {
+        val crashlyticsKit = Crashlytics.Builder()
+                .core(CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build()
+
+        Fabric.with(this, crashlyticsKit)
     }
 }
