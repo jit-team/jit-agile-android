56091e7ea6080033f98f05d30136ca2c41259847
Marek Kondracki <marek.kondracki@jitsolutions.pl>
2018-11-19 17:18:52 +0100

    unnecessary variable assignment

--- app/src/main/java/pl/jitsolutions/agile/domain/usecases/ObserveDailyUseCase.kt
+++ app/src/main/java/pl/jitsolutions/agile/domain/usecases/ObserveDailyUseCase.kt
@@ -2,7 +2,6 @@ package pl.jitsolutions.agile.domain.usecases
 
 import kotlinx.coroutines.experimental.CoroutineDispatcher
 import kotlinx.coroutines.experimental.channels.ProducerScope
-import kotlinx.coroutines.experimental.channels.ReceiveChannel
 import kotlinx.coroutines.experimental.channels.consumeEach
 import pl.jitsolutions.agile.domain.Daily
 import pl.jitsolutions.agile.domain.Response
@@ -14,11 +13,8 @@ class ObserveDailyUseCase(
 ) :
     ChannelUseCase<ObserveDailyUseCase.Params, Daily?>(dispatcher) {
 
-    private lateinit var observeDaily: ReceiveChannel<Response<Daily?>>
-
     override suspend fun ProducerScope<Response<Daily?>>.build(params: Params) {
-        observeDaily = dailyRepository.observeDaily(params.dailyId)
-        observeDaily.consumeEach {
+        dailyRepository.observeDaily(params.dailyId).consumeEach {
             send(it)
         }
     }
