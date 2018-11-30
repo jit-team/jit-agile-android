77bf5630fc1d4f235402846ad6130a9863b5f058
Sebastian Staszynski <sebastian.staszynski@jitsolutions.pl>
2018-11-30 12:19:44 +0100

    fix test

--- app/src/test/java/pl/jitsolutions/agile/domain/usecases/UserLoginUseCaseTest.kt
+++ app/src/test/java/pl/jitsolutions/agile/domain/usecases/UserLoginUseCaseTest.kt
@@ -1,5 +1,6 @@
 package pl.jitsolutions.agile.domain.usecases
 
+import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import kotlinx.coroutines.Dispatchers
@@ -7,7 +8,9 @@ import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import pl.jitsolutions.agile.assertThat
 import pl.jitsolutions.agile.common.Error
+import pl.jitsolutions.agile.domain.User
 import pl.jitsolutions.agile.domain.response
+import pl.jitsolutions.agile.repository.NotificationRepository
 import pl.jitsolutions.agile.repository.UserRepository
 
 class UserLoginUseCaseTest {
@@ -19,10 +22,23 @@ class UserLoginUseCaseTest {
                 login("abc@email.pl", "abc")
             } doReturn
                 response(Unit)
+
+            onBlocking {
+                getLoggedInUser()
+            } doReturn
+                response<User?>(User("", "", ""))
+        }
+
+        val mockNotificationsRepository = mock<NotificationRepository> {
+            onBlocking {
+                assignDeviceTokenToUser(any())
+            } doReturn
+                response(Unit)
         }
 
         val params = UserLoginUseCase.Params("abc@email.pl", "abc")
-        val useCase = UserLoginUseCase(mockUserRepository, Dispatchers.Default)
+        val useCase =
+            UserLoginUseCase(mockUserRepository, mockNotificationsRepository, Dispatchers.Default)
 
         val response = useCase.executeAsync(params).await()
 
@@ -34,9 +50,11 @@ class UserLoginUseCaseTest {
     @Test
     fun `empty login`() = runBlocking {
         val mockUserRepository = mock<UserRepository>()
+        val mockNotificationsRepository = mock<NotificationRepository>()
 
         val params = UserLoginUseCase.Params("", "abc")
-        val useCase = UserLoginUseCase(mockUserRepository, Dispatchers.Default)
+        val useCase =
+            UserLoginUseCase(mockUserRepository, mockNotificationsRepository, Dispatchers.Default)
 
         val response = useCase.executeAsync(params).await()
 
@@ -48,9 +66,11 @@ class UserLoginUseCaseTest {
     @Test
     fun `empty password`() = runBlocking {
         val mockUserRepository = mock<UserRepository>()
+        val mockNotificationsRepository = mock<NotificationRepository>()
 
         val params = UserLoginUseCase.Params("abc@test.pl", "")
-        val useCase = UserLoginUseCase(mockUserRepository, Dispatchers.Default)
+        val useCase =
+            UserLoginUseCase(mockUserRepository, mockNotificationsRepository, Dispatchers.Default)
 
         val response = useCase.executeAsync(params).await()
 
